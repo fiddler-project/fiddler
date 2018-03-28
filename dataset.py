4d15a3a1eea092475aa0948c6e25d7cc536f6e18
Manthan Thakar <manthant15@gmail.com>
2018-03-28 17:05:38 -0400

    Add new class variable

--- dataset.py
+++ dataset.py
@@ -1,11 +1,13 @@
 class Dataset(object):
 
-    def __init__(self, path):
+    def __init__(self, path, batch_size=1000):
         """ `path` is processed file's path """
         self.path = path
+        self.batch_size = batch_size
 
     def get_data(self, format="raw"):
         pass
 
-    def batch(self, size=100):
+    def batch(self):
+        """ Returns a new batch """
         pass
