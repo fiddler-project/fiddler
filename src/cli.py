bdaaa5c09b68d29f44137da51f13e8b64bfdc92e
dwarakarashmi <dwarakarashmi@ccs.neu.edu>
2018-04-12 23:05:49 -0400

    dropout feature addition

--- src/cli.py
+++ src/cli.py
@@ -21,6 +21,7 @@ def main():
 @click.option("--learning-rate", "-r", default=1e-3, type=click.FLOAT, help="Learning Rate")
 @click.option("--num-steps", "-n", type=click.INT, default=15, help="No. of time steps in RNN")
 @click.option("--cell-size", "-s", type=click.INT, default=100, help="Dimension of cell states")
+@click.option("--dropout", "-d", type=click.FLOAT, help="Dropout probability for the output")
 @click.option("--epochs", "-e", type=click.INT,
               help="No. of epochs to run training for")
 @click.option("--cell", "-c", type=click.Choice(['lstm', 'gru']),
@@ -28,12 +29,12 @@ def main():
 @click.option("--test-seed", "-t", help="Seed input for printing predicted text after each training step")
 @click.option("--delim/--no-delim", default=True, help="Delimit tunes with start and end symbol")
 def train_rnn(file, batch_size, layers, learning_rate,
-              num_steps, cell_size, epochs, cell, test_seed, delim):
+              num_steps, cell_size, dropout, epochs, cell, test_seed, delim):
     """ Train neural network """
     ds = Dataset(file, batch_size=batch_size,
                  num_steps=num_steps, with_delim=delim)
     n = RNN(data=ds, cell=cell, num_layers=layers,
-            learning_rate=learning_rate, cell_size=cell_size, num_epochs=epochs)
+            learning_rate=learning_rate, cell_size=cell_size, dropout=dropout, num_epochs=epochs)
     n.train(test_output=True, test_seed=test_seed, with_delim=delim)
 
 
