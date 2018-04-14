5b26a9a4844edc3a79774a60a82a85950d8e50ec
Manthan Thakar <manthant15@gmail.com>
2018-04-14 02:04:07 -0400

    Update google cloud script

--- gc.sh
+++ gc.sh
@@ -1,6 +1,4 @@
 sudo apt-get install python-pip -y
 sudo apt-get install htop -y
-git clone https://github.com/fiddler-project/fiddler.git
-cd fiddler/
 pip install -r requirements.txt
 sudo python setup.py install
