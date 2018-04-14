7628eae0975445430bbf30b3e07108f3af619415
Manthan Thakar <manthant15@gmail.com>
2018-04-14 01:33:47 -0400

    Add default parameter for dropout in cli

--- src/cli.py
+++ src/cli.py
@@ -21,7 +21,7 @@ def main():
 @click.option("--learning-rate", "-r", default=1e-3, type=click.FLOAT, help="Learning Rate")
 @click.option("--num-steps", "-n", type=click.INT, default=15, help="No. of time steps in RNN")
 @click.option("--cell-size", "-s", type=click.INT, default=100, help="Dimension of cell states")
-@click.option("--dropout", "-d", type=click.FLOAT, help="Dropout probability for the output")
+@click.option("--dropout", "-d", type=click.FLOAT, default=0.8, help="Dropout probability for the output")
 @click.option("--epochs", "-e", type=click.INT,
               help="No. of epochs to run training for")
 @click.option("--cell", "-c", type=click.Choice(['lstm', 'gru']),
@@ -29,8 +29,8 @@ def main():
 @click.option("--test-seed", "-t", help="Seed input for printing predicted text after each training step")
 @click.option("--delim/--no-delim", default=True, help="Delimit tunes with start and end symbol")
 @click.option("--save/--no-save", default=True, help="Save model to file")
-def train_rnn(file, batch_size, layers, learning_rate, dropout, 
-  num_steps, cell_size, epochs, cell, test_seed, delim, save):
+def train_rnn(file, batch_size, layers, learning_rate, dropout,
+              num_steps, cell_size, epochs, cell, test_seed, delim, save):
     """ Train neural network """
     model_name = "cell-{}-size-{}-batch-{}-steps-{}-layers-{}-lr-{}-dropout-{}".format(
         cell, cell_size, batch_size, num_steps, layers, learning_rate, dropout)
